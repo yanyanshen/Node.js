964eda94e1640d1438508894fd97be9116d398fc
yanyanshen <18736199128@163.com>
2017-09-26 14:15:08 +0800

    nodejs v8.5.0

---
+++ Node.js/serverjs/server.js
@@ -0,0 +1,22 @@
+//var http = require('http');
+//http.createServer(function(req,res){
+//    res.writeHead(200,{'Content-Type':'text/plain'});
+//    res.end('Hello World\n');
+//}).listen(1337,'127.0.0.1');
+//
+
+
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer(function(req, res){
+    res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+res.end('Hello World\n');
+}).listen(3000,'127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:3000 ');
+
