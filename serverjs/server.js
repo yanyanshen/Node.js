6e3990f08d21b29777f1f628ecdd1152960d7899
yanyanshen <18736199128@163.com>
2017-09-26 14:39:09 +0800

    修改代码

--- serverjs/server.js
+++ serverjs/server.js
@@ -8,11 +8,11 @@
 
 
 const http = require('http');
+//
+//const hostname = '127.0.0.1';
+//const port = 3000;
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer(function(req, res){
+http.createServer(function(req, res){
     res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
 res.end('Hello World\n');
